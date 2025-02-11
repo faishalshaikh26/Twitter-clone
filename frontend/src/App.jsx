58ef53fd69765a5cbac09b7e5ea89ff58cdfcca3
Faishal shaikh <faishalshaikh625@gmail.com>
2025-02-11 15:06:48 +0530

    2.4 Notification

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,10 @@ import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/home/HomePage'
 import SignUpPage from './pages/auth/signup/SignUpPage'
 import LoginPage from './pages/auth/login/LoginPage'
+import NotificationPage from './pages/notification/NotificationPage'
+import ProfilePage from './pages/profile/ProfilePage'
 import Sidebar from './components/common/Sidebar' 
+import RightPanel from './components/common/RightPanel'
 
 const App = () => {
   return (
@@ -13,7 +16,10 @@ const App = () => {
         <Route path='/' element={<HomePage />} />
         <Route path='/signup' element={<SignUpPage />} />
         <Route path='/login' element={<LoginPage />} />
+        <Route path='/notifications' element={<NotificationPage />} />
+        <Route path='/profile/:username' element={<ProfilePage />} />
       </Routes>
+      <RightPanel/>
     </div>
   )
 }
